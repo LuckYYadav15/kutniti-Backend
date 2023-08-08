11b43aa71c194e3cc705714da1e50aac7c652830
LuckYYadav15 <20bcs077@iiitdwd.ac.in>
2023-08-08 16:25:35 +0530

    cron import added

--- routes/countryRouter.js
+++ routes/countryRouter.js
@@ -1,6 +1,7 @@
 const countryController = require("../controllers/countryController");
 
 const router = require("express").Router();
+const cron = require("node-cron");
 
 cron.schedule("10 0 * * *", () => {
   countryController.allcountryArticles();
